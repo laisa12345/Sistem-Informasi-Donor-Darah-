521f3415a9d47f97efb956b9b9afa6bca858f512
Laisa Imama Fajarina <152072473+laisa12345@users.noreply.github.com>
2025-06-06 23:50:14 +0700

    Add files via upload

---
+++ Task4/DonorDarah.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Bab4;
+
+/**
+ *
+ * @author waka I
+ */
+    public abstract class DonorDarah {
+    // Atribut dasar yang dimiliki semua donor darah
+    private String golonganDarah;
+    private int tanggalDonorTerakhir;
+    
+    // Constructor
+    public DonorDarah() {
+        this.golonganDarah = "Unknown";
+        this.tanggalDonorTerakhir = 0;
+    }
+    
+    // Method umum untuk semua donor darah
+    public abstract boolean bolehDonor();
+    
+    // Getter dan setter
+    public String getGolonganDarah() {
+        return golonganDarah;
+    }
+    
+    public void setGolonganDarah(String golonganDarah) {
+        this.golonganDarah = golonganDarah;
+    }
+    
+    public int getTanggalDonorTerakhir() {
+        return tanggalDonorTerakhir;
+    }
+    
+    public void setTanggalDonorTerakhir(int tanggalDonorTerakhir) {
+        this.tanggalDonorTerakhir = tanggalDonorTerakhir;
+    }
+}
